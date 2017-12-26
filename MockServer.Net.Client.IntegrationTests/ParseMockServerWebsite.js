879d0b8deb1c2fb136a50ce32e7cd71f7928cb13
Tiago Coutinho Monteiro <tiago.monteiro@farfetch.com>
2017-12-26 15:35:27 +0000

    Parse tests

--- MockServer.Net.Client.IntegrationTests/ParseMockServerWebsite.js
+++ MockServer.Net.Client.IntegrationTests/ParseMockServerWebsite.js
@@ -2,3 +2,5 @@
 for (var i = 0; i < elements.length; i++) {
     elements[i].textContent;
 }
+
+var requestBodyElements = document.querySelectorAll("div.panel.title > div.panel > div.panel:nth-child(2) > pre > code > span.str:nth-child(1)");
