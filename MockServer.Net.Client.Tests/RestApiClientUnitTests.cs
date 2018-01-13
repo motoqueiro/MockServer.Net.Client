1e6e657c79a6ade885e18dc9f800e8360370a4f7
Tiago Coutinho Monteiro <tiago.monteiro@farfetch.com>
2018-01-13 21:54:49 +0000

    Unit tests changes

--- MockServer.Net.Client.Tests/RestApiClientUnitTests.cs
+++ MockServer.Net.Client.Tests/RestApiClientUnitTests.cs
@@ -30,6 +30,155 @@ namespace MockServer.Net.Client.Tests
             this._client = new RestApiClient(this._serverUrl);
         }
 
+        [Category("Expectation")]
+        [TestCase(201, "expectation created", TestName = "CreateExpectation_ShouldReturnCreated")]
+        [TestCase(400, "incorrect request format", TestName = "CreateExpectation_ShouldReturnBadRequest")]
+        [TestCase(406, "invalid expectation", TestName = "CreateExpectation_ShouldReturnNotAcceptable")]
+        public async Task CreateExpectationRequestTest(
+            HttpStatusCode status,
+            string description)
+        {
+            //Arrange
+            this._httpTest.RespondWith(string.Empty, (int)status);
+            var jsonData = this._fixture.Generate<string>();
+
+            //Act
+            var response = await this._client.Expectation(jsonData);
+
+            //Assert
+            AssertResponse(response, status, description);
+        }
+
+        [Category("Reset")]
+        [TestCase(200, "expectations and recorded requests cleared", TestName = "ResetRequest_ShouldReturnOk")]
+        public async Task ResetRequestTest(
+            HttpStatusCode status,
+            string description)
+        {
+            //Arrange
+            this._httpTest.RespondWith(string.Empty, (int)status);
+
+            //Act
+            var response = await this._client.Reset();
+
+            //Assert
+            AssertResponse(response, status, description);
+        }
+
+        //TODO: Define retieve format and retrieve type for unit tests
+        [Category("Expectation")]
+        [TestCase(200, "recorded requests or active expectations returned", TestName = "RetrieveRequest_ShouldReturnOk")]
+        [TestCase(400, "incorrect request format", TestName = "RetrieveRequest_ShouldReturBadRequest")]
+        public async Task RetrieveRequestTest(
+            RetrieveFormatEnum format,
+            RetrieveTypeEnum type,
+            HttpStatusCode status,
+            string description)
+        {
+            //Arrange
+            this._httpTest.RespondWith(string.Empty, (int)status);
+            var jsonData = this._fixture.Generate<string>();
+
+            //Act
+            var response = await this._client.Retrieve(
+                jsonData,
+                format,
+                type);
+
+            //Assert
+            AssertResponse(response, status, description);
+            this._httpTest
+                .ShouldHaveCalled(this._serverUrl + "retrieve")
+                .WithoutQueryParamValue("format", format)
+                .WithoutQueryParamValue("type", type)
+                .WithRequestJson(jsonData)
+                .WithVerb(HttpMethod.Put);
+        }
+
+        [Category("Status")]
+        [TestCase(200, "MockServer is running and listening on the listed ports", TestName = "StatusRequest_ShouldReturnOk")]
+        public async Task StatusRequestTest(
+            HttpStatusCode status,
+            string description)
+        {
+            //Arrange
+            this._httpTest.RespondWith(string.Empty, (int)status);
+
+            //Act
+            var response = await this._client.Status();
+
+            //Assert
+            AssertResponse(response, status, description);
+            this._httpTest
+                .ShouldHaveCalled(this._serverUrl + "status")
+                .WithVerb(HttpMethod.Put);
+        }
+
+        [Category("Stop")]
+        [TestCase(200, "MockServer process is stopping", TestName = "StopRequest_ShouldReturnOk")]
+        public async Task StopRequestTest(
+            HttpStatusCode status,
+            string description)
+        {
+            //Arrange
+            this._httpTest.RespondWith(string.Empty, (int)status);
+
+            //Act
+            var response = await this._client.Stop();
+
+            //Assert
+            AssertResponse(response, status, description);
+            this._httpTest
+                .ShouldHaveCalled(this._serverUrl + "stop")
+                .WithVerb(HttpMethod.Put);
+        }
+
+        [Category("Verify")]
+        [TestCase(200, "matching request has been received specified number of times", TestName = "VerifyRequest_ShouldReturnOk")]
+        [TestCase(400, "incorrect request format", TestName = "VerifyRequest_ShouldReturnBadRequest")]
+        [TestCase(406, "request has not been received specified numbers of times", TestName = "VerifyRequest_ShouldReturnNotAcceptable")]
+        public async Task VerifyRequestTest(
+            HttpStatusCode status,
+            string description)
+        {
+            //Arrange
+            this._httpTest.RespondWith(string.Empty, (int)status);
+            var jsonData = this._fixture.Generate<string>();
+
+            //Act
+            var response = await this._client.Verify(jsonData);
+
+            //Assert
+            AssertResponse(response, status, description, jsonData);
+            this._httpTest
+                .ShouldHaveCalled(this._serverUrl + "verify")
+                .WithRequestJson(jsonData)
+                .WithVerb(HttpMethod.Put);
+        }
+
+        [Category("VerifySequence")]
+        [TestCase(202, "request sequence has been received in specified order", TestName = "VerifySequenceRequest_ShouldReturnAccepted")]
+        [TestCase(400, "request has not been received specified numbers of times", TestName = "VerifySequenceRequest_ShouldReturnBadRequest")]
+        [TestCase(406, "request has not been received specified numbers of times", TestName = "VerifySequenceRequest_ShouldReturnNotAcceptable")]
+        public async Task VerifySequenceRequestTest(
+            HttpStatusCode status,
+            string description)
+        {
+            //Arrange
+            this._httpTest.RespondWith(string.Empty, (int)status);
+            var jsonData = this._fixture.Generate<string>();
+
+            //Act
+            var response = await this._client.VerifySequence(jsonData);
+
+            //Assert
+            AssertResponse(response, status, description, jsonData);
+            this._httpTest
+                .ShouldHaveCalled(this._serverUrl + "verifysequence")
+                .WithRequestJson(jsonData)
+                .WithVerb(HttpMethod.Put);
+        }
+
         [TestCaseSource(typeof(UnitTestCaseData), "OkTestData")]
         public async Task GenericClientAction_ShouldBeOk(
             string methodName,
@@ -102,27 +251,19 @@ namespace MockServer.Net.Client.Tests
             return parametersList;
         }
 
-        private async Task<Response> Invoke(
-            string methodName,
-            IEnumerable<object> parameters)
-        {
-            var type = this._client.GetType();
-            var methodInfo = type.GetMethod(methodName);
-            return await (Task<Response>)methodInfo.Invoke(
-                this._client,
-                parameters.ToArray());
-        }
-
         private void AssertResponse(
             Response response,
             HttpStatusCode status,
             string description,
-            string body)
+            string body = null)
         {
             response.Should().NotBeNull();
             response.Code.Should().Equals(status);
             response.Description.Should().Equals(description);
-            response.Content.Should().Equals(body);
+            if (!string.IsNullOrEmpty(body))
+            {
+                response.Content.Should().Equals(body); 
+            }
         }
     }
 }
--- MockServer.Net.Client.Tests/UnitTestCaseData.cs
+++ MockServer.Net.Client.Tests/UnitTestCaseData.cs
@@ -11,39 +11,7 @@
             get
             {
                 var fixture = new Fixture();
-                yield return new TestCaseData("Expectation", 201, "expectation created", string.Empty, new[] { fixture.Generate<string>() })
-                     .SetName("CreateExpectation_ShouldReturnCreated")
-                     .SetCategory("Expectation");
-                yield return new TestCaseData("Expectation", 400, "incorrect request format", string.Empty, new[] { fixture.Generate<string>() })
-                    .SetName("CreateExpectation_ShouldReturnBadRequest")
-                    .SetCategory("Expectation");
-                yield return new TestCaseData("Expectation", 406, "invalid expectation", string.Empty, new[] { fixture.Generate<string>() })
-                    .SetName("CreateExpectation_ShouldReturnNotAcceptable")
-                    .SetCategory("Expectation");
-                yield return new TestCaseData("Reset", 200, "expectations and recorded requests cleared", string.Empty, null, null)
-                    .SetName("ResetRequest_ShouldReturnOk")
-                    .SetCategory("Reset");
-                yield return new TestCaseData("Retrieve", 200, "recorded requests or active expectations returned", null)
-                    .SetName("RetrieveRequest_ShouldReturnOk")
-                    .SetCategory("Retrieve");
-                yield return new TestCaseData("Retrieve", 400, "incorrect request format")
-                    .SetName("RetrieveRequest_ShouldReturBadRequest")
-                    .SetCategory("Retrieve");
-                yield return new TestCaseData("Status", 200, "MockServer is running and listening on the listed ports")
-                    .SetName("StatusRequest_ShouldReturnOk")
-                    .SetCategory("Status");
-                yield return new TestCaseData("Stop", 200, "MockServer process is stopping")
-                    .SetName("StopRequest_ShouldReturnOk")
-                    .SetCategory("Stop");
-                yield return new TestCaseData("Verify", 200, "matching request has been received specified number of times", string.Empty)
-                    .SetName("VerifyRequest_ShouldReturnOk")
-                    .SetCategory("Verify");
-                yield return new TestCaseData("Verify", 400, "incorrect request format", string.Empty)
-                    .SetName("VerifyRequest_ShouldReturnBadRequest")
-                    .SetCategory("Verify");
-                yield return new TestCaseData("Verify", 406, "request has not been received specified numbers of times", string.Empty)
-                    .SetName("VerifyRequest_ShouldReturnNotAcceptable")
-                    .SetCategory("Verify");
+               
                 yield return new TestCaseData("VerifySequence", 202, "request sequence has been received in specified order", string.Empty)
                     .SetName("VerifySequenceRequest_ShouldReturnAccepted")
                     .SetCategory("VerifySequence");
